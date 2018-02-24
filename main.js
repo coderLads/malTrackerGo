3759da7aad9e6746762259a085afa6914f4955f9
Milo Rupp <badtzmaru139@gmail.com>
2018-02-24 01:38:59 -0800

    made top bar drag

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function createWindow() {
     mainWindow = new BrowserWindow({
         width: 1000,
         height: 700,
-        frame: true,
+        frame: false,
         icon: __dirname + '/images/icon.png',
     })
 
